f0d882c0804103137beeae38f13bd926e9cb935d
Uboh Chukwuemeke <ubohjrchukwuemeke@gmail.com>
2024-03-22 19:49:24 +0100

    mordified waether Api App

--- app.js
+++ app.js
@@ -1,27 +1,42 @@
+require('dotenv').config()
+
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    
-      const url ="https://api.openweathermap.org/data/2.5/weather?q=London&appid=a449ebe1698f4002a9e56faa6225ff02&units=metric";
-      https.get(url, function (response) {
-        response.on("data", function (data) {
-          const weatherData = JSON.parse(data);
-          const temp = weatherData.main.temp;
-          const weatherDiscription = weatherData.weather[0].description;
-          const icon = weatherData.weather[0].icon
-          const imgUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+app.use(bodyParser.urlencoded({ extended: true }));
 
-          res.write("")
-          res.write("<p> The weather ias cureently "  + weatherDiscription  + "</p>")
-          res.write("<h1>the temperature of Lodon is " + temp + " degree celcius.</h1>");
-          res.write("<img src=" + imgUrl + ">")
-          res.send();
-        });
-      });
-  
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => { 
+
+   const querry = req.body.cityName;
+   const apikey = process.env.API_KEY;
+   const unit = "metric"
+
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" + querry +"&appid=" + apikey +"&units="+ unit +"";
+  https.get(url, function (response) {
+    response.on("data", function (data) {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherDiscription = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imgUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+      res.write("");
+      res.write("<p> The weather is cureently " + weatherDiscription + "</p>");
+      res.write(
+        "<h1>the temperature of " + querry + " is " + temp + " degree celcius.</h1>"
+      );
+      res.write("<img src=" + imgUrl + ">");
+      res.send();
+    });
+  });
 });
 
 app.listen(port, () => {
